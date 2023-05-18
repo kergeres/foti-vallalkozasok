8e53128f82792e67a8024b6f29fcd3a56593ec5e
David Papp <pappdavidlevele@gmail.com>
2023-05-18 17:23:58 +0200

    next

--- src/components/api/firebaseApis.js
+++
@@ -1,6 +0,0 @@
-
-import axios from "axios";
-
-export default axios.create({
-    baseURL: "https://fova-7f8dd-default-rtdb.europe-west1.firebasedatabase.app/"
-})
--- src/components/helpers/codeToValue.jsx
+++
@@ -1,9 +0,0 @@
-import cvts from "../../data/cvts";
-
-const codeToValue = (code, fromObject) => {
-  return cvts[fromObject].find((item) => {
-    return item.code === code;
-  }).value;
-};
-
-export default codeToValue;
